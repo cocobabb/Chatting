0449b3564dfae8a04055ff892aefeee687bd2a0a
cocobabb <cocobab05108@gmail.com>
2025-05-15 17:02:19 +0900

    [feat] [fe]채팅방 기존메세지 및 실시간 메세지 처리

--- FE/src/pages/ChatRoom.jsx
+++ FE/src/pages/ChatRoom.jsx
@@ -1,14 +1,22 @@
 import axios from "axios";
 import React, { useEffect, useRef, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function ChatRoom() {
+  const navigate = useNavigate();
+
   const token = localStorage.getItem("token");
   const wsRef = useRef(null);
+
+  const [preMsg, setPreMsg] = useState([]);
+  const [date, setDate] = useState(new Set());
+
   const [messages, setMessages] = useState([]);
   const scrollRef = useRef(null);
   const [input, setInput] = useState("");
 
+  // useNavigate state에 보낸 값 가져오기
+  // id: 채팅방 id  title: 채팅방 이름
   const location = useLocation();
   const { id, title } = location.state || {};
 
@@ -17,16 +25,35 @@ export default function ChatRoom() {
       navigate("/");
       return;
     }
+    const getChatMessages = async () => {
+      const response = await axios.get(
+        import.meta.env.VITE_API_URL + `/find/chat/list/${id}`,
+        { headers: { Authorization: "Bearer " + token } }
+      );
+      console.log(response);
+      setPreMsg(response.data);
+    };
+
+    getChatMessages();
+  }, [id, token]);
+
+  // 웹소켓 연결 및 메세지 렌더링
+  useEffect(() => {
     console.log(id);
+
     const ws = new WebSocket(
       `ws://${import.meta.env.VITE_WS_URL}/${id}?token=${token}`
     );
     wsRef.current = ws;
 
     ws.onmessage = (e) => {
-      const [sender, ...rest] = e.data.split(": ");
-      const text = rest.join(": ");
+      console.log("on");
+      console.log(e.data);
+      const [sender, ...rest] = e.data.split(":"); // split은 :을 기준으로 : 앞 요소는 변수로 : 뒤 요소는 배열로 만듦
+      const text = rest.join(""); // 배열에 ""문자열을 더해서 문자열로 만듦
 
+      console.log("sender:", sender);
+      console.log("text:", text);
       setMessages((prev) => [...prev, { sender, text }]);
     };
 
@@ -54,9 +81,21 @@ export default function ChatRoom() {
         <div className="max-w-2xl mx-auto mt-10 p-4 border rounded-lg shadow bg-white">
           <h2 className="text-2xl font-bold mb-4">{title}</h2>
           <div className="h-80 overflow-y-auto border p-3 rounded mb-4 bg-gray-50">
+            <div>{date}</div>
+
+            {preMsg.map((data, i) => (
+              <div key={i} className="mb-2">
+                <span className="font-semibold">
+                  {data?.username ?? "익명"}
+                </span>
+                :{data?.content ?? "내용x"}
+              </div>
+            ))}
+
             {messages.map((msg, i) => (
               <div key={i} className="mb-2">
-                <span className="font-semibold">{msg.sender}</span>: {msg.text}
+                <span className="font-semibold">{msg?.sender ?? "익명"}</span>:
+                {msg?.text ?? "텍스트 없음"}
               </div>
             ))}
             <div ref={scrollRef}></div>
