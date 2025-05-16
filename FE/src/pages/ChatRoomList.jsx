1ededbc80db58ad1ed880f772f52ebc972fc6d1a
cocobabb <cocobab05108@gmail.com>
2025-05-17 00:41:12 +0900

    [feat] [fe]채팅방 생성

--- FE/src/pages/ChatRoomList.jsx
+++ FE/src/pages/ChatRoomList.jsx
@@ -6,8 +6,11 @@ import ChatRoom from "./ChatRoom";
 export default function ChatRoomList() {
   const token = localStorage.getItem("token");
   const username = localStorage.getItem("username");
+
   const [chatRooms, setChatRooms] = useState([]);
   const navigate = useNavigate();
+
+  const [input, setInput] = useState("");
 
   useEffect(() => {
     if (!token) {
@@ -31,6 +34,31 @@ export default function ChatRoomList() {
     getChatRoomList();
   }, []);
 
+  // 입력 값 제어
+  const handleInput = (e) => {
+    const chatRoomTitle = e.target.value;
+    if (chatRoomTitle.trim() === "" && chatRoomTitle !== "") return;
+
+    setInput(chatRoomTitle);
+  };
+
+  const makeChatRoom = async () => {
+    console.log("채팅룸 만들기 입장");
+    if (!input) return;
+    console.log("채팅룸 만들기 조건문 이후");
+    try {
+      const response = await axios.post(
+        import.meta.env.VITE_API_URL + "/create",
+        { title: input, username },
+        { headers: { Authorization: "Bearer " + token } }
+      );
+      setChatRooms([...chatRooms, response.data]);
+      setInput(""); // 입력창 초기화
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   function moveToChatRoom(id, title) {
     title = title.replaceAll(" ", "");
     // encodeURIComponent(String) : 어떤 언어든 안전하게 URL로 변환(URL에서 title 꺠지는 거 방지)
@@ -46,6 +74,23 @@ export default function ChatRoomList() {
       <div className="text-3xl font-bold text-center mt-10 text-green-700">
         채팅방 목록
       </div>
+
+      <div className="flex justify-center m-5">
+        <input
+          className="w-80 border rounded px-3 py-2"
+          placeholder="채팅방 이름을 입력하세요..."
+          value={input}
+          onChange={handleInput}
+          onKeyDown={(e) => e.key === "Enter" && makeChatRoom()}
+        />
+        <button
+          className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+          onClick={makeChatRoom}
+        >
+          채팅방 생성
+        </button>
+      </div>
+
       <div className="w-full h-screen flex justify-center">
         <div className="w-full max-w-md space-y-4 mt-8">
           {chatRooms.map((room) => (
