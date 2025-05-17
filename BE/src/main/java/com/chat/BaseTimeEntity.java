ad2c0358b9e57be43922d84634947e45d1ef6eff
cocobabb <cocobab05108@gmail.com>
2025-05-18 05:10:37 +0900

    [fix] [be]Reactor Core 기반은 자동으로 @CreatedDate 등이 인식이 안되서 수동으로 설정

--- BE/src/main/java/com/chat/BaseTimeEntity.java
+++ BE/src/main/java/com/chat/BaseTimeEntity.java
@@ -5,19 +5,24 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.util.Date;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
+import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
+@Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
-    @CreatedDate
-    @Column(updatable = false)
+    @Column(nullable = false, updatable = false)
     private Date createdAt;
 
-    @LastModifiedDate
+    @Column(nullable = false)
     private Date updatedAt;
+
+
+    public BaseTimeEntity() {
+        this.createdAt = new Date();
+        this.updatedAt = new Date();
+    }
 }
